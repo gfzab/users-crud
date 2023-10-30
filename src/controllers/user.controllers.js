8b71d17e77e9051c82d9feda5a56d8556fed636b
Gabriel Zabala <140466880+Gabrielzabala93@users.noreply.github.com>
2023-10-30 09:21:36 -0500

    First commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,57 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    // Operaciones...
+    const users = await User.findAll()
+    return res.json(users);
+});
+
+const create = catchError(async(req, res) => {
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.create({
+        first_name,
+        last_name,
+        email,
+        password,
+        birthday,
+    });
+    return res.status(201).json(user);
+});
+
+//obtener id
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    const user = await User.findByPk(id)
+    return res.json(user);
+});
+
+// delete /:id
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: { id: id } });
+    return res.sendStatus(204);
+});
+
+//actualizar id
+
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const {first_name, last_name, email, password, birthday } = req.body;
+    const user = await User.update(
+        { first_name, last_name, email, password, birthday },
+        { where: { id: id }, returning: true }
+    );
+    return res.json(user);
+})
+
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update,
+
+}
---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+   // En Mayúsculas y singular      // en minúsculas y singular
+const User = sequelize.define('user', {
+    // Definimos las columnas aquí
+
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATE, 
+        allowNull: false 
+    },
+   
+});
+
+module.exports = User;
